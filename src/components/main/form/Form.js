561e4d13d78795bca4322e924b04b8b63b0e1b0e
adrianlehmann <info@adrianlehmann.net>
2017-12-03 00:19:26 +0100

    Refactored code (code quality & old naming) and fixed few ui resizing issues

--- src/components/main/form/Form.js
+++ src/components/main/form/Form.js
@@ -1,196 +1,177 @@
 import React from "react";
-import { TextField, DatePicker, DropDownMenu, MenuItem } from 'material-ui'
+import {DatePicker, DropDownMenu, MenuItem, TextField} from 'material-ui'
 import areIntlLocalesSupported from 'intl-locales-supported'
+
 const Immutable = require('immutable');
 
 export default class Home extends React.Component {
 
-  constructor(props) {
-    super(props)
+    /* TODO: Localize & internationalize */
+    constructor(props) {
+        super(props);
 
-    let DateTimeFormat
-    if (areIntlLocalesSupported(['de'])) {
-      DateTimeFormat = global.Intl.DateTimeFormat;
-    } else {
-      const IntlPolyfill = require('intl');
-      DateTimeFormat = IntlPolyfill.DateTimeFormat;
-      require('intl/locale-data/jsonp/de');
+        let DateTimeFormat;
+        if (areIntlLocalesSupported(['de'])) {
+            DateTimeFormat = global.Intl.DateTimeFormat;
+        } else {
+            const IntlPolyfill = require('intl');
+            DateTimeFormat = IntlPolyfill.DateTimeFormat;
+            require('intl/locale-data/jsonp/de');
+        }
+
+        this.state = {
+            company_name: '',
+            controlledDate: null,
+            language: 0,
+            DateTimeFormat: DateTimeFormat,
+            locale: 'de',
+            source: 0,
+        }
     }
 
-    this.state = {
-      company_name: '',
-      controlledDate: null,
-      value: 1,
-      DateTimeFormat: DateTimeFormat,
-      locale: 'de'
+    componentDidMount() {
     }
-  }
 
-  componentDidMount() {
-  }
+    componentWillUnmount() {
+    }
 
-  componentWillUnmount() {
-  }
+    handleChange = (event, date) => {
+        this.setState({
+            controlledDate: date,
+        })
+    };
 
-  handleChange = (event, date) => {
-    this.setState({
-      controlledDate: date,
-    })
-  }
+    _handleLanguageChange = (event, index, value) => this.setState({language: value});
 
-  _handleLanguageChange = (event, index, value) => this.setState({value})
+    _handleSourceChange = (event, index, value) => this.setState({source: value});
 
-  render() {
-    return (
-      <div className="db pt3">
-        <div className="dib pa4 v-top">
-          <p className="bf">Zieldaten</p>
-          <p className="bf f6 tl pt3" style={{marginBottom: '0'}}>Für:</p>
-          <DropDownMenu value={this.state.value}
-                        selectedMenuItemStyle={{color: '#A63324'}}
-                        onChange={this._handleLanguageChange}>
-            <MenuItem value={1} primaryText="Hack & Söhne" />
-            <MenuItem value={2} primaryText="talKIT" />
-          </DropDownMenu>
-          <p className="bf f6 tl pt3" style={{marginBottom: '0'}}>Zielsprache:</p>
-          <DropDownMenu value={this.state.value}
-                        selectedMenuItemStyle={{color: '#A63324'}}
-                        onChange={this._handleLanguageChange}>
-            <MenuItem value={1} primaryText="Deutsch" />
-            <MenuItem value={2} primaryText="Englisch" />
-          </DropDownMenu>
-          <br />
-        </div>
-        <div className="dib pa4">
-          <p className="bf">Addresse des Unternehmens</p>
-          <TextField
-            hintText="Name des Unternehmens"
-            inputStyle={{
-              fontFamily: "Source Sans Pro",
-              color: '#A63324'
-            }}
-            hintStyle={{
-              fontFamily: "Source Sans Pro",
-            }}
-            errorText={this.state.company_name}
-          /><br />
-          <TextField
-            hintText="z.H. von"
-            inputStyle={{
-              fontFamily: "Source Sans Pro",
-              color: '#A63324'
-            }}
-            hintStyle={{
-              fontFamily: "Source Sans Pro",
-            }}
-            errorText={this.state.company_name}
-          /><br/>
-          <TextField
-            hintText="Adresse"
-            inputStyle={{
-              fontFamily: "Source Sans Pro",
-              color: '#A63324'
-            }}
-            hintStyle={{
-              fontFamily: "Source Sans Pro",
-            }}
-            errorText={this.state.company_name}
-          /><br/>
-          <div className="db">
-            <TextField
-              className="dib"
-              hintText="PLZ"
-              style={{
-                width: '56px'
-              }}
-              inputStyle={{
-                fontFamily: "Source Sans Pro",
-                color: '#A63324'
-              }}
-              hintStyle={{
-                fontFamily: "Source Sans Pro",
-              }}
-              errorText={this.state.company_name}
-            />
-            <TextField
-              className="dib"
-              hintText="Stadt"
-              style={{
-                width: '190px',
-                paddingLeft: '6px'
-              }}
-              inputStyle={{
-                fontFamily: "Source Sans Pro",
-                color: '#A63324'
-              }}
-              hintStyle={{
-                fontFamily: "Source Sans Pro",
-              }}
-              errorText={this.state.company_name}
-            /><br />
-          </div>
-          <TextField
-            hintText="Land (optional)"
-            inputStyle={{
-              fontFamily: "Source Sans Pro",
-              color: '#A63324'
-            }}
-            hintStyle={{
-              fontFamily: "Source Sans Pro",
-            }}
-            errorText={this.state.company_name}
-          /><br />
-        </div>
-      <div className="dib pa4 v-top">
-        <p className="bf">Daten</p>
-        <DatePicker
-          hintText="Rechungsdatum"
-          hintStyle={{
-            fontFamily: "Source Sans Pro",
-          }}
-          inputStyle={{
-            fontFamily: "Source Sans Pro",
-            color: '#A63324'
-          }}
-          value={this.state.controlledDate}
-          onChange={this.handleChange}
-          DateTimeFormat={this.state.DateTimeFormat}
-          locale={this.state.locale}
-        /><br />
-        <DatePicker
-          hintText="Kooperationsleistungsdatum"
-          inputStyle={{
-            fontFamily: "Source Sans Pro",
-            color: '#A63324'
-          }}
-          hintStyle={{
-            fontFamily: "Source Sans Pro",
-          }}
-          floatingLabelText={"Wann hast du die Zusage bekommen?"}
-          floatingLabelFixed={true}
-          floatingLabelStyle={{color: "#212121", width:'275px'}}
-          errorText={this.state.company_name}
-          DateTimeFormat={this.state.DateTimeFormat}
-          locale={this.state.locale}
-        /><br />
-        <DatePicker
-          hintText="Überweisungsdatum"
-          inputStyle={{
-            fontFamily: "Source Sans Pro",
-            color: '#A63324'
-          }}
-          hintStyle={{
-            fontFamily: "Source Sans Pro",
-          }}
-          floatingLabelText={"Bis wann muss das Geld überwiesen sein?"}
-          floatingLabelFixed={true}
-          floatingLabelStyle={{color: "#212121", width:'306px'}}
-          errorText={this.state.company_name}
-          DateTimeFormat={this.state.DateTimeFormat}
-          locale={this.state.locale}
-        /><br />
-      </div>
-      </div>
-    )
-  }
+
+    render() {
+        const {hsText, hsFont, hsColor} = styles;
+        return (
+            <div className
+                     ="db pt3">
+                <div className="dib pa4 v-top">
+                    <p className="bf">Zieldaten</p>
+                    <p className="bf f6 tl pt3" style={{marginBottom: '0'}}>Für:</p>
+                    <DropDownMenu value={this.state.source}
+                                  style={{hsColor, width: 200 /* TODO adapt to largest item */}}
+                                  selectedMenuItemStyle={hsColor}
+                                  onChange={this._handleSourceChange}>
+                        /* TODO: Make values configable */
+                        <MenuItem value={0} primaryText="Hack & Söhne"/>
+                        <MenuItem value={1} primaryText="talKIT"/>
+                    </DropDownMenu>
+                    <p className="bf f6 tl pt3" style={{marginBottom: '0'}}>Zielsprache:</p>
+                    <DropDownMenu value={this.state.language}
+                                  style={{hsColor, width: 200 /* TODO adapt to largest item */}}
+                                  selectedMenuItemStyle={hsColor}
+                                  onChange={this._handleLanguageChange}>
+                        <MenuItem value={0} primaryText="Deutsch"/>
+                        <MenuItem value={1} primaryText="Englisch"/>
+                    </DropDownMenu>
+                    <br/>
+                </div>
+                <div className="dib pa4">
+                    <p className="bf">Addresse des Unternehmens</p>
+                    <TextField
+                        hintText="Name des Unternehmens"
+                        inputStyle={hsText}
+                        hintStyle={hsFont}
+                        errorText={this.state.company_name}
+                    /><br/>
+                    <TextField
+                        hintText="z.H. von"
+                        inputStyle={hsText}
+                        hintStyle={hsFont}
+                        errorText={this.state.company_name}
+                    /><br/>
+                    <TextField
+                        hintText="Adresse"
+                        inputStyle={hsText}
+                        hintStyle={hsFont}
+                        errorText={this.state.company_name}
+                    /><br/>
+                    <div className="db">
+                        <TextField
+                            className="dib"
+                            hintText="PLZ"
+                            style={{
+                                width: '56px'
+                            }}
+                            inputStyle={hsText}
+                            hintStyle={hsFont}
+                            errorText={this.state.company_name}
+                        />
+                        <TextField
+                            className="dib"
+                            hintText="Stadt"
+                            style={{
+                                width: '190px',
+                                paddingLeft: '6px'
+                            }}
+                            inputStyle={hsText}
+                            hintStyle={hsFont}
+                            errorText={this.state.company_name}
+                        /><br/>
+                    </div>
+                    <TextField
+                        hintText="Land (optional)"
+                        inputStyle={hsText}
+                        hintStyle={hsFont}
+                        errorText={this.state.company_name}
+                    /><br/>
+                </div>
+                <div className="dib pa4 v-top">
+                    <p className="bf">Daten</p>
+                    <DatePicker
+                        hintText="Rechungsdatum"
+                        inputStyle={hsText}
+                        hintStyle={hsFont}
+                        value={this.state.controlledDate}
+                        onChange={this.handleChange}
+                        DateTimeFormat={this.state.DateTimeFormat}
+                        locale={this.state.locale}
+                    /><br/>
+                    <DatePicker
+                        hintText="Kooperationsleistungsdatum"
+                        inputStyle={hsText}
+                        hintStyle={hsFont}
+                        floatingLabelText={"Wann hast du die Zusage bekommen?"}
+                        floatingLabelFixed={true}
+                        floatingLabelStyle={{color: "#212121", width: '275px'}}
+                        errorText={this.state.company_name}
+                        DateTimeFormat={this.state.DateTimeFormat}
+                        locale={this.state.locale}
+                    /><br/>
+                    <DatePicker
+                        hintText="Überweisungsdatum"
+                        inputStyle={hsText}
+                        hintStyle={hsFont}
+                        floatingLabelText={"Bis wann muss das Geld überwiesen sein?"}
+                        floatingLabelFixed={true}
+                        floatingLabelStyle={{color: "#212121", width: '306px'}}
+                        errorText={this.state.company_name}
+                        DateTimeFormat={this.state.DateTimeFormat}
+                        locale={this.state.locale}
+                    /><br/>
+                </div>
+            </div>
+        )
+    }
 }
+
+const styles = {
+    hsFont: {
+        fontFamily: "Source Sans Pro",
+    },
+    hsColor: {
+        color: "#A63324",
+    },
+    hsText: {
+        color: "#A63324",
+        fontFamily: "Source Sans Pro",
+    },
+
+
+};
